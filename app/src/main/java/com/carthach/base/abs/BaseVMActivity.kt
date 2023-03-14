1ec507bbf4396054bcbc2e123efb8f7fff6a8cc0
carthach <carthach@163.com>
2023-03-14 09:27:28 +0800

    update BaseVMActivity

--- app/src/main/java/com/carthach/base/abs/BaseVMActivity.kt
+++ app/src/main/java/com/carthach/base/abs/BaseVMActivity.kt
@@ -1,8 +1,40 @@
 package com.carthach.base.abs
 
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseVMActivity<VB: ViewBinding,VM: BaseViewModel> :BaseActivity<VB>() {
+    protected open lateinit var mViewModel: VM
+
+    protected abstract fun initViewModel(): Class<VM>
+    protected abstract fun initObserve()
+    protected abstract fun loginInvalid()
+    protected abstract fun showLoading()
+    protected abstract fun dismissLoading()
+
+    override fun init() {
+        mViewModel = ViewModelProvider(this)[initViewModel()]
+        observe()
+    }
+
+    private fun observe() {
+        mViewModel.apply {
+            isShowLoading.observe(this@BaseVMActivity) {
+                if (it) {
+                    showLoading()
+                } else {
+                    dismissLoading()
+                }
+            }
+            loginInvalid.observe(this@BaseVMActivity) {
+                if (it) {
+                    loginInvalid()
+                }
+            }
+        }
+
+        initObserve()
+    }
 
 
 }
